e49884de1860299cf40c15a38f499247bdac4072
trachselr <trachselr@bzz.ch>
2022-10-03 10:43:16 +0200

    feat: task 1.3

--- 1.3/present/example2.js
+++ 1.3/present/example2.js
@@ -1,1 +1,12 @@
-alert("Hallo Welt");
+document.addEventListener('DOMContentLoaded', () => { // dient 
+    alert("Hallo Welt");
+
+    function clicked() {
+        document.write('Hallo Welt');
+    };
+
+    const button = document.getElementById('button');
+    const buttonTwo = document.getElementById('buttonTwo')
+    buttonTwo.addEventListener('click', () => alert("test"));
+    button.addEventListener('click', clicked); // clicked ohne () am schluss
+});
